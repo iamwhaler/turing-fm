f8f5a0e15d9669d3f3d5dcc5272e239e85f27c88
Maxim <iamwhaler8@gmail.com>
2019-06-13 01:46:32 +0300

    Rules for auto generation

--- src/knowledge/rules.js
+++ src/knowledge/rules.js
@@ -3,7 +3,7 @@ const _ENDPOINT_URL = "https://small-gm-api.herokuapp.com/sequence";
 export const rules = {
   tickRule: {
     onTick: (store, params) => {
-      if (store.tick % 4 === 0) {
+      if (store.tick % 10 === 0) {
         params.helpers.requestSequence(_ENDPOINT_URL, params.gin)
       }
       return store;
