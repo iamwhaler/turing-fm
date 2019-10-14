4cd969003bd2a9cde02c78269ef6e1505e9f4897
Naddyson <Naddyson777@gmail.com>
2019-10-14 23:45:18 +0300

    console log remove

--- src/components/solar_system/SolarSystem.js
+++ src/components/solar_system/SolarSystem.js
@@ -29,22 +29,11 @@ class SolarSystem extends React.Component {
         let sorted_orbits_indexes = _.sortBy(solar_system.orbits, "time_length").filter(o => !o.empty).map((o) => {
             return _.findIndex(solar_system.orbits, orbit => o === orbit);
         });
-        console.log("sorted_orbits_indexes")
-        console.log(sorted_orbits_indexes)
         let orbits = _.map(solar_system.orbits, (orbit, i) => {
             //ищем позицию текущей орбиты в отсортированных индексах
             let sorted_index = _.findIndex(sorted_orbits_indexes, n => n === i);
-            console.log("i="+i)
-            console.log("sortedIndex")
-            console.log(sorted_index)
             let prev_time_length = sorted_index ? sorted_index - 1 : null;
-            console.log("prev_time_length")
-            console.log(prev_time_length)
-            console.log("time_length orbit")
-            console.log(orbit.time_length)
             let orbit_r = calcOrbitRadius(orbit.time_length, prev_time_length, sorted_index);
-            console.log("orbit_r")
-            console.log(orbit_r)
             let data = {
                 container_size: container_size,
                 system_id: solar_system.id,
@@ -61,9 +50,6 @@ class SolarSystem extends React.Component {
                 </Orbit>
             )
         });
-        console.log("orbits")
-        console.log(orbits)
-        console.log("RENDER SOLAR SYSTEM")
         return (
             <div className="solar-system" style={{ width: container_size, height: container_size}}>
                 {create_planet_button}
